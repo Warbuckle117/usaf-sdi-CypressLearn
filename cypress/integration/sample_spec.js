88196549be7875e46052ced13c886f489ac07d72
Warbuckle117 <warbuckle1993@outlook.com>
2021-07-29 11:45:52 -0400

    refresh

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,8 @@
+const { isTypedArray } = require("util/types");
+const { describe } = require("yargs");
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  })
